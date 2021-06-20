3ee11bb89eb17f7ee9163fab6670973f176b7152
jubalthang <jubalthang@gamil.com>
2021-06-19 18:21:17 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, msg) {
+    let array = [];
+    for (let i=0; i<names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${msg} gift!`);
+    }
+    return array;
+}
+
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
